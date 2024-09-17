43f678bbc85f162dff5d22b6e27a9370943e3a89
Aaron Levine <ald.levine@gmail.com>
2024-09-17 12:01:49 -0400

    [gdextension] initialize library

---
+++ gdextension/gdextension.cpp
@@ -0,0 +1,40 @@
+#include <godot_cpp/godot.hpp>
+
+namespace rptr::gdextension
+{
+    void initialize_extension(godot::ModuleInitializationLevel p_level)
+    {
+        if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+        {
+            return;
+        }
+    }
+
+    void terminate_extension(godot::ModuleInitializationLevel p_level)
+    {
+        if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE)
+        {
+            return;
+        }
+    }
+
+    extern "C"
+    {
+        GDExtensionBool rptr_library_init(
+            GDExtensionInterfaceGetProcAddress p_get_proc_address,
+            const GDExtensionClassLibraryPtr p_library,
+            GDExtensionInitialization *r_initialization)
+        {
+            godot::GDExtensionBinding::InitObject init_obj(
+                p_get_proc_address, p_library, r_initialization);
+
+            init_obj.register_initializer(initialize_extension);
+            init_obj.register_terminator(terminate_extension);
+            init_obj.set_minimum_library_initialization_level(
+                godot::MODULE_INITIALIZATION_LEVEL_SCENE);
+
+            return init_obj.init();
+        }
+    }
+
+} //namespace rptr::gdextension
