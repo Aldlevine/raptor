51be6af5fec8eee36bb11c3c92aa7907422982ef
Aaron Levine <ald.levine@gmail.com>
2024-09-17 11:42:09 -0400

    [raptor] add integer division methods to math module

---
+++ raptor/math/integer_division.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "vector.hpp"
+
+namespace rptr::math
+{
+    template <std::integral Int>
+    Int floor_div(Int p_lhs, Int p_rhs)
+    {
+        Int result = p_lhs / p_rhs;
+        if (p_lhs % p_rhs < 0)
+        {
+            --result;
+        }
+        return result;
+    }
+
+    template <std::integral Int, size_t t_size>
+    math::Vector<Int, t_size> floor_div(
+        const math::Vector<Int, t_size> &p_lhs,
+        const math::Vector<Int, t_size> &p_rhs)
+    {
+        return [&]<size_t... t_it>(std::index_sequence<t_it...>)
+        {
+            return math::Vector<Int, t_size>{ floor_div(p_lhs[t_it], p_rhs[t_it])... };
+        }(std::make_index_sequence<t_size>());
+    }
+
+    template <std::integral Int>
+    Int ceil_div(Int p_lhs, Int p_rhs)
+    {
+        Int result = p_lhs / p_rhs;
+        if (p_lhs % p_rhs > 0)
+        {
+            ++result;
+        }
+        return result;
+    }
+
+    template <std::integral Int, size_t t_size>
+    math::Vector<Int, t_size> ceil_div(
+        const math::Vector<Int, t_size> &p_lhs,
+        const math::Vector<Int, t_size> &p_rhs)
+    {
+        return [&]<size_t... t_it>(std::index_sequence<t_it...>)
+        {
+            return math::Vector<Int, t_size>{ ceil_div(p_lhs[t_it], p_rhs[t_it])... };
+        }(std::make_index_sequence<t_size>());
+    }
+} // namespace rptr::math
--- raptor/math/math.hpp
+++ raptor/math/math.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 #include "aabb.hpp"
+#include "integer_division.hpp"
 #include "vector.hpp"
