702346394f7ee23f7d3d7af4b83af717a6550577
Aaron Levine <ald.levine@gmail.com>
2024-09-20 14:39:32 -0400

    [raptor.grid] (WIP) update GridChunk to use GridCell and expose iter/index methods

--- raptor/grid/grid_chunk.hpp
+++ raptor/grid/grid_chunk.hpp
@@ -1,20 +1,29 @@
 #pragma once
 
+#include "grid_cell.hpp"
 #include "grid_defs.hpp"
 
 namespace rptr::grid
 {
-    template <typename Type_>
+    /// @brief A statically sized chunk.
+    /// @tparam t_Unit: The unit type (value type of each channel)
+    /// @tparam t_channels: The number of channels
+    template <UnitType t_Unit, size_t t_channels>
     class GridChunk
     {
     public:
-        using Type = Type_;
-        using Data = std::array<Type, CHUNK_SIZE.product()>;
+        using m_Unit = t_Unit;
+        constexpr static size_t m_channels = t_channels;
 
-        const Data &data() const { return m_data; }
-        Data &data() { return m_data; }
+        using m_Cell = GridCell<m_Unit, m_channels>;
+        using m_Data = std::array<m_Cell, CHUNK_SIZE.product()>;
+
+        constexpr auto begin(this auto &&self) { return self.m_data.begin(); }
+        constexpr auto end(this auto &&self) { return self.m_data.end(); }
+
+        constexpr auto &operator[](this auto &&self, size_t index) { return self.m_data[index]; }
 
     private:
-        Data m_data{};
+        m_Data m_data{};
     };
 } // namespace rptr::grid
